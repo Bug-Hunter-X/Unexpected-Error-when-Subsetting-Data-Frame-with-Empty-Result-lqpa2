54aeb130c098b25f9c6a5d3fa926364aebea4b70
BugHunter-X <askzenai@gmail.com>
2025-01-30 05:58:06 +0100

    Add bug.R file

---
+++ bug.R
@@ -0,0 +1,10 @@
+```R
+# This code attempts to subset a data frame based on a condition that results in an empty subset.
+# However, instead of returning an empty data frame, it produces an error.
+
+df <- data.frame(a = 1:5, b = letters[1:5])
+
+subset_df <- df[df$a > 10, ]
+
+print(subset_df)
+```
